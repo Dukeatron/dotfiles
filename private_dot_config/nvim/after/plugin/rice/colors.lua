d88a22e3070570bd6c8a50088cd6cddab6e882cd
Dukeatron2000 <shreyas_bhandari@yahoo.in>
2022-12-04 11:19:45 +0530

    colour theme fix (neovim)

--- private_dot_config/nvim/after/plugin/rice/colors.lua
+++ private_dot_config/nvim/after/plugin/rice/colors.lua
@@ -1,18 +1,18 @@
 vim.g.catppuccin_flavour = "mocha" -- latte, frappe, macchiato, mocha
 
-local darkmode_overrides = {
-  base = "#000000",
-  crust = "#000000",
-  mantle = "#000000",
+--local darkmode_overrides = {
+  --base = "#000000",
+  --crust = "#000000",
+ -- mantle = "#000000",
   -- surface0 = "#101010",
   -- surface1 = "#141414",
   -- surface2 = "#181818",
   -- overlay1 = "#202020",
   -- overlay2 = "#242424",
   -- overlay3 = "#282828",
-}
+--}
 
-local colors = require("catppuccin.palettes").get_palette()
+--local colors = require("catppuccin.palettes").get_palette()
 
 require("catppuccin").setup({
   	transparent_background = true,
@@ -20,12 +20,6 @@ require("catppuccin").setup({
 	integrations = {
 	  fidget = true,
 	  bufferline = true
-	},
-
-	color_overrides = {
-	  mocha = darkmode_overrides,
-	  macchiato = darkmode_overrides,
-	  frappe = darkmode_overrides
 	}
 })
 
